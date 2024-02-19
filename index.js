6f9decffd8e102b4c73ed91755282f4bf09f0f7b
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-02-19 15:07:53 +0100

    Push console.log

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const name = prompt("Write your name here");
-const surname = prompt("Write your surname here");
+const firstName = prompt("Write your name here");
+const lastName = prompt("Write your surname here");
 const color = prompt("What's your favorite color?");
+console.log(firstName, lastName, color);
+const password = firstName + lastName + color;
+console.log(password);
