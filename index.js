0d690aa03e7382cbd1a18d9ed1b7b30ba46d0051
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-02-19 18:14:52 +0100

    aggiunto numero

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const firstName = prompt("Write your name here");
 const lastName = prompt("Write your surname here");
 const color = prompt("What's your favorite color?");
 console.log(firstName, lastName, color);
-const password = firstName + lastName + color;
+const password = firstName + lastName + color + 24;
 console.log(password);
 document.getElementById('code').innerHTML = password;
